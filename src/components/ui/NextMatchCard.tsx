b9ba6b87c0623a5719a5ecb47fddcbe0c9da10c4
NunoF <n.fernandes.contact@gmail.com>
2025-12-23 22:38:42 +0100

    feat: add NextMatchCard component to display upcoming match details.

--- src/components/ui/NextMatchCard.tsx
+++ src/components/ui/NextMatchCard.tsx
@@ -40,11 +40,12 @@ export default function NextMatchCard() {
       </CardContent>
 
       <CardFooter className="flex flex-col items-start justify-center gap-2">
-        <p className="flex items-center justify-center w-full gap-6 mb-16">
+        <p className="flex items-center justify-center w-full gap-6 mb-6">
           <span className="text-3xl font-bold">AZIAS FC</span>
           <span className="text-secondary text-xl">VS</span>
           <span className="text-3xl font-bold">Rival FC</span>
         </p>
+
         <p className="text-xl flex items-center gap-3">
           <Calendar className="inline" />
           Domingo, 14 de Dezembro de 2025
