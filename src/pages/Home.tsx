0128d13c4cff838e4a78a63165a5ecd82419e07e
NunoF <n.fernandes.contact@gmail.com>
2025-12-30 20:46:11 +0100

    feat: add new Home page with hero, match, team, about, sponsors, and map sections

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -37,6 +37,19 @@ export function Home() {
       <Container className="mx-3 lg:mx-16 my-16 lg:my-16">
         <HomePageSponsorsSection />
       </Container>
+
+      <Container className="mx-3 lg:mx-16 my-16 lg:my-16">
+        <div className=""></div>
+
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2975.3848121857786!2d-8.365324423425053!3d41.77691977174226!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd250f08fdba3cb3%3A0xc741083a73dd31e1!2sCampo%20de%20Futebol%20da%20Manguela!5e0!3m2!1sfr!2spt!4v1767123579062!5m2!1sfr!2spt"
+          style={{ border: 0 }}
+          allowFullScreen
+          loading="lazy"
+          referrerPolicy="no-referrer-when-downgrade"
+          className="w-full h-[350px]"
+        ></iframe>
+      </Container>
     </>
   );
 }
