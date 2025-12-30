b9cd69d7a6f37d54f67af7648edcc58962417a63
NunoF <n.fernandes.contact@gmail.com>
2025-12-30 05:48:22 +0100

    feat: Add `NavBar` component for site navigation.

--- src/components/layout/NavBar.tsx
+++ src/components/layout/NavBar.tsx
@@ -6,34 +6,33 @@ import { navLinks } from "@/config/constants/constants";
 
 export function Navbar() {
   return (
-    <nav className="h-full w-full">
+    <nav className="h-full w-full flex flex-col items-center">
       {/* ----- Logo ----- */}
       <Link to="/">
         <img
           src="/azias-fc-logo.png"
           alt="Logo da equipa FC Azias"
-          className="w-24 h-24 rounded-full"
+          className="w-48 h-48 rounded-full"
         />
         {/* <span className="font-bold text-xl text-gray-900">Azias FC</span> */}
       </Link>
 
       {/* ---------- Desktop Nav ---------- */}
-      <div className="lg:h-2/3">
-        <div className="flex flex-col items-start justify-center gap-6 h-full">
-          {navLinks.map(({ href, label, id }) => (
-            <NavLink
-              key={id}
-              to={href}
-              className={({ isActive }) =>
-                `font-semibold text-md hover:text-primary hover:bg-white px-3 py-3 w-full ease-in-out duration-500 ${
-                  isActive ? "bg-background text-primary" : "text-white"
-                }`
-              }
-            >
-              {label.toUpperCase()}
-            </NavLink>
-          ))}
-        </div>
+
+      <div className="flex flex-col items-center justify-center gap-6">
+        {navLinks.map(({ href, label, id }) => (
+          <NavLink
+            key={id}
+            to={href}
+            className={({ isActive }) =>
+              `font-semibold text-md hover:text-primary hover:bg-white px-3 py-3 w-full ease-in-out duration-500 rounded ${
+                isActive ? "bg-background text-primary" : "text-white"
+              }`
+            }
+          >
+            {label.toUpperCase()}
+          </NavLink>
+        ))}
       </div>
     </nav>
   );
