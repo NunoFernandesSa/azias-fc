916d3ca8477d80bb925cc2a9dcbb62b141f36ef4
NunoF <n.fernandes.contact@gmail.com>
2025-12-31 01:27:57 +0100

    feat: add HomePageTeamSection component to display team information.

--- src/components/features/team/HomePageTeamSection.tsx
+++ src/components/features/team/HomePageTeamSection.tsx
@@ -5,13 +5,12 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/index";
-import { Trophy, User } from "lucide-react";
 import { Link } from "react-router-dom";
 
 export function HomePageTeamSection() {
   const title = "A nossa equipa";
   return (
-    <Card className="relative bg-primary/60 text-primary-foreground min-h-[300px] hover:bg-transparent transition-all duration-500">
+    <Card className="relative bg-primary/80 text-primary-foreground min-h-[300px] hover:bg-transparent transition-all duration-500">
       <img
         src="/team.jpg"
         alt=""
@@ -25,12 +24,16 @@ export function HomePageTeamSection() {
 
       <CardFooter className=" flex-1 flex-col items-start justify-end gap-6 mt-12">
         <div className="text-xl md:text-2xl font-bold flex items-center gap-2">
-          <User className="mr-2 rounded-full bg-secondary p-2" size={40} />
-          27 Jogadores
+          ⚽ Espírito de equipa
         </div>
         <div className="text-xl md:text-2xl font-bold flex items-center gap-2">
-          <Trophy className="mr-2 rounded-full bg-secondary p-2" size={40} />
-          Campeonato INATEL
+          🤝 Respeito
+        </div>
+        <div className="text-xl md:text-2xl font-bold flex items-center gap-2">
+          🔥 Paixão local
+        </div>
+        <div className="text-xl md:text-2xl font-bold flex items-center gap-2">
+          🏆 Campeonato INATEL
         </div>
         <Button asChild variant={"secondary"} className="w-full sm:w-1/2">
           <Link to="/equipa" className="text-start">
