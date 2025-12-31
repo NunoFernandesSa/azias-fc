4920cff4ab30211f47f44011c0af83b74b94b460
NunoF <n.fernandes.contact@gmail.com>
2025-12-31 01:53:52 +0100

    feat: Add new Footer component and Inatel club logo.

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -3,19 +3,33 @@ import { Button } from "@/components/index.ts";
 export function Footer() {
   return (
     <footer className="lg:ml-48 flex-1 bg-primary text-primary-foreground">
-      <div className="container mx-auto h-16 flex items-center justify-between">
+      <div className="flex flex-col items-center gap-3 py-6">
+        <p className="text-xl">Clube amador INATEL</p>
+        <img src="/logo-inatel.webp" alt="Logo Inatel" className="w-36" />
+      </div>
+
+      {/* <div className="flex items-center justify-center gap-6">
+        <Link to="https://www.facebook.com/aziasfc" target="_blank">
+          <FaFacebook size={30} />
+        </Link>
+
+        <Link to="https://www.instagram.com/aziasfc" target="_blank">
+          <FaInstagram size={30} />
+        </Link>
+      </div> */}
+
+      <div className="container mx-auto h-16 flex flex-col md:flex-row items-center justify-between">
         <p className="text-sm">
-          &copy; {new Date().getFullYear()} Azias FC - Tous droits reservés
+          &copy; {new Date().getFullYear()} Azias FC - Todos os direitos
+          reservados
         </p>
 
-        <div className="">
-          <Button
-            variant="link"
-            className="text-sm font-light cursor-pointer text-primary-foreground ease-in-out transition-all duration-500 hover:text-primary-foreground/70"
-          >
-            Termos e Condições
-          </Button>
-        </div>
+        <Button
+          variant="link"
+          className="text-sm font-light cursor-pointer text-primary-foreground ease-in-out transition-all duration-500 hover:text-primary-foreground/70"
+        >
+          Termos e Condições
+        </Button>
       </div>
     </footer>
   );
