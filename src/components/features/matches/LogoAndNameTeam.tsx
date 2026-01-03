8480be2063f71867076c6efcc53b72663aa8d36c
NunoF <n.fernandes.contact@gmail.com>
2026-01-03 16:11:30 +0100

    feat: Add `MatchCard` and `LogoAndNameTeam` components to display match details.

--- src/components/features/matches/LogoAndNameTeam.tsx
+++ src/components/features/matches/LogoAndNameTeam.tsx
@@ -12,7 +12,9 @@ export function LogoAndNameTeam({
         alt={"logo da equipa " + name}
         className="w-36 md:w-40 rounded-full shadow shadow-secondary/20"
       />
-      <span className="text-base font-bold">{name}</span>
+      <span className="text-base font-bold text-primary-foreground">
+        {name}
+      </span>
     </div>
   );
 }
--- src/components/features/matches/MatchCard.tsx
+++ src/components/features/matches/MatchCard.tsx
@@ -1,10 +1,20 @@
 // ---------- components ----------
-import { Card, CardContent, LogoAndNameTeam } from "@/components/index.ts";
+import {
+  Card,
+  CardBackground,
+  CardContent,
+  LogoAndNameTeam,
+} from "@/components/index.ts";
 // ---------- icons ----------
 import { Calendar, Timer } from "lucide-react";
 // ---------- types ----------
 import type { MatchCardProps } from "@/types/index.ts";
 import type { JSX } from "react";
+
+// ---------- images ----------
+import BgWebp from "/images/optimized/bg-match-card.webp";
+import BgJpg from "/images/optimized/bg-match-card.jpg";
+import BgMobileWebp from "/images/optimized/bg-match-card-mobile.webp";
 
 /**
  * MatchCard component
@@ -20,42 +30,50 @@ export function MatchCard({
   results,
 }: MatchCardProps): JSX.Element {
   return (
-    <Card className="bg-primary text-primary-foreground relative pt-1">
-      <CardContent className="flex flex-col items-center gap-6">
-        {/* Date and hour */}
-        <div className="w-full flex flex-col sm:flex-row items-center justify-center text-secondary gap-3">
-          <span className="text-sm italic">{match.jornada}° Jornada</span>
-          <span className="flex items-center gap-2">
-            <Calendar className="w-4 h-4" /> {match.date}
-          </span>
-          <span className="flex items-center gap-2">
-            <Timer className="w-4 h-4" />
-            {match.hour}H
-          </span>
-        </div>
-
-        {/* Teams */}
-        <div className="flex flex-1 items-center justify-center gap-6">
-          <LogoAndNameTeam
-            logo={team?.teamPlayingAtHomeLogo || ""}
-            name={team?.teamPlayingAtHome || ""}
-          />
-          <span className="text-xl font-bold italic text-secondary">VS</span>
-          <LogoAndNameTeam
-            logo={team?.teamPlayingAwayLogo || ""}
-            name={team?.teamPlayingAway || ""}
-          />
-        </div>
-
-        {/* Result */}
-        {results && (
-          <div className="flex items-center gap-2 text-2xl font-bold">
-            <span>{results.teamPlayingAwayScore}</span>
-            <span>-</span>
-            <span>{results.teamPlayingAtHomeScore}</span>
+    <CardBackground
+      src={BgJpg}
+      webpSrc={BgWebp}
+      mobileSrc={BgMobileWebp}
+      overlay={true}
+      overlayColor="bg-primary/60"
+    >
+      <Card className="bg-gradient-to-r from-primary to-primary/40 rounded-lg pt-1">
+        <CardContent className="flex flex-col items-center gap-6">
+          {/* Date and hour */}
+          <div className="w-full flex flex-col sm:flex-row items-center justify-center text-secondary gap-3">
+            <span className="text-sm italic">{match.jornada}° Jornada</span>
+            <span className="flex items-center gap-2">
+              <Calendar className="w-4 h-4" /> {match.date}
+            </span>
+            <span className="flex items-center gap-2">
+              <Timer className="w-4 h-4" />
+              {match.hour}H
+            </span>
           </div>
-        )}
-      </CardContent>
-    </Card>
+
+          {/* Teams */}
+          <div className="flex flex-1 items-center justify-center gap-6">
+            <LogoAndNameTeam
+              logo={team?.teamPlayingAtHomeLogo || ""}
+              name={team?.teamPlayingAtHome || ""}
+            />
+            <span className="text-xl font-bold italic text-secondary">VS</span>
+            <LogoAndNameTeam
+              logo={team?.teamPlayingAwayLogo || ""}
+              name={team?.teamPlayingAway || ""}
+            />
+          </div>
+
+          {/* Result */}
+          {results && (
+            <div className="flex items-center gap-2 text-2xl font-bold text-primary-foreground">
+              <span>{results.teamPlayingAwayScore}</span>
+              <span className="text-secondary">-</span>
+              <span>{results.teamPlayingAtHomeScore}</span>
+            </div>
+          )}
+        </CardContent>
+      </Card>
+    </CardBackground>
   );
 }
