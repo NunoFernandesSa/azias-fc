f1bfbaba1fc47a749e54b91b8a2af7b624869325
NunoF <n.fernandes.contact@gmail.com>
2026-01-03 16:05:03 +0100

    feat: add initial Home page component with hero, next match, team, about, sponsors, and contact sections.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -26,9 +26,23 @@ export function Home() {
           title="Próximo Jogo"
           cardContent={<NextMatchCardContent {...calendar[0]} />}
           cardFooter={
-            <Button asChild variant={"secondary"} className="w-full">
-              <Link to="/calendario">Consultar calendário</Link>
-            </Button>
+            <div className="flex flex-col md:flex-row items-center justify-center gap-2 w-full">
+              <Button asChild variant={"secondary"} className="w-full md:w-1/2">
+                <Link to="/calendario">Consultar calendário</Link>
+              </Button>
+              <Button
+                asChild
+                variant={"outline"}
+                className="w-full md:w-1/2 bg-transparent hover:bg-white/80 text-primary-foreground hover:text-primary"
+              >
+                <Link
+                  to="https://desporto.inatel.pt/pt/tournament/1322383/ranking"
+                  target="_blank"
+                >
+                  Consultar classificação
+                </Link>
+              </Button>
+            </div>
           }
           className="bg-primary text-primary-foreground "
         />
